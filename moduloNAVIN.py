e300b75e4104d3fbcb809aca82cbd291766134a2
j-iann <87838923+j-iann@users.noreply.github.com>
2025-04-02 04:24:14 -0300

    QUE ÓDIO DESSA PORRA

--- moduloNAVIN.py
+++ moduloNAVIN.py
@@ -1,8 +1,39 @@
+#Imports
 import pygame
+from moduloNAVIN import Navin
 
-#navin class
-class NAVIN:
-    def __init__(self, numeroNavin, navinLista):
-        self.image = pygame.image.load(navinLista[numeroNavin])  # Carregar imagem do jogador
-        self.image = pygame.transform.scale(self.image, (267, 315))  # Ajustar o tamanho da imagem
-        self.rect = self.image.get_rect(center=(720, 150))  # Usar o retângulo da imagem
+#Comando pygame (NAO TOQUE)
+pygame.init()
+
+#Tela 
+WIDTH, HEIGHT = 1440, 810
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Top-Down Shooter")
+clock = pygame.time.Clock()
+running = True
+
+navin = Navin((720, 150))
+
+# Main game loop
+while running:
+
+
+    clock.tick(30)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # Carregar a imagem de fundo
+    background = pygame.image.load("spritesGT/Map_1.png")
+    background = pygame.transform.scale(background, (WIDTH, HEIGHT))  # Ajustar o tamanho da imagem do fundo
+
+    navin.movement(WIDTH)
+    navin.animation()
+
+    screen.blit(background, (0, 0))
+    screen.blit(navin.image, navin.rect)
+
+    #Comando pygame (NAO TOQUE)
+    pygame.display.flip()
+    clock.tick(30)
+
+pygame.quit()
