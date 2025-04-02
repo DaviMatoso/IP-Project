d0f537b11f63e975a6abc335efb0673cb6748c1a
j-iann <87838923+j-iann@users.noreply.github.com>
2025-04-02 03:47:32 -0300

    arrumando a classe do chefe pra depois adicionar o novo ataque

--- moduloBarraDeVida.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-#barra de vida class
-class barraDeVida:
-    def __init__(self, danado):
-        self.rect = pygame.Rect(318, 736, 800-(danado*0.266666666), 25) #esse 0.26 so ta ai se a vida do navin for 3000 (800/vida)
-
-        
