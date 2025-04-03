ff64a569366330dedd879098df4bf91cbe1906e6
j-iann <87838923+j-iann@users.noreply.github.com>
2025-04-03 11:03:36 -0300

    ataque fase 2 navin finalizado ((maybe???))

--- moduloAtaquesNavin.py
+++ moduloAtaquesNavin.py
@@ -9,21 +9,21 @@ class Ability(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (30, 30))
         self.rect = self.image.get_frect(midbottom = pos)
 
-    def shoot(pos, sprite_groups, num_projectiles = 7, spacing_vertical = 40, spacing_horizontal = 20):
-        # Vertical line (|)
+    def shoot(pos, sprite_groups, num_projectiles = 7, spacing_vertical = 40, spacing_horizontal = 25, spacing_first_projectile = 2):
+        # Linha vertical (|)
         for i in range(num_projectiles):
             y = pos[1] + i * spacing_vertical
             Ability((pos[0], y), sprite_groups)
 
-        # Left diagonal (/)
-        for i in range(1, num_projectiles):
-            x = pos[0] - i * spacing_horizontal
+        # Linha diagonal esquerda (/)
+        for i in range(num_projectiles):
+            x = pos[0] - (i + spacing_first_projectile) * spacing_horizontal
             y = pos[1] + i * spacing_vertical
             Ability((x, y), sprite_groups)
 
-        # Right diagonal (\)
-        for i in range(1, num_projectiles):
-            x = pos[0] + i * spacing_horizontal
+        # Linha diagonal direita (\)
+        for i in range(num_projectiles):
+            x = pos[0] + (i + spacing_first_projectile) * spacing_horizontal
             y = pos[1] + i * spacing_vertical
             Ability((x, y), sprite_groups)
 
